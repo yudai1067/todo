07491eaa834044fd67eeb0e6a0fa38e71d12b822
KURONECOPC\kuron <yudai1067@gmail.com>
2024-11-19 13:15:54 +0900

    todosテーブル修正

--- src/database/migrations/2024_11_17_131939_create_todos_table.php
+++ src/database/migrations/2024_11_17_131939_create_todos_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->string('title', 100);
             $table->text('description')->nullable();
             $table->dateTime('deadline');
